13b7a7e9f5338a5b020daca1fc206169a65393c2
Hetty de Vries <hetty82@gmail.com>
2017-11-12 16:40:48 +0100

    Setup layout, header and stickies components - add stickies route - add css reset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router'
+
+import { appRoutes } from './routes'
 
 import { AppComponent } from './app.component'
 import { TestComponent } from './test/test.component'
 import { Test2Component } from './test2/test2.component'
+import { StickiesComponent } from './stickies/stickies.component'
+import { LayoutComponent } from './layout/layout.component'
+import { HeaderComponent } from './header/header.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    Test2Component
+    Test2Component,
+    StickiesComponent,
+    LayoutComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router'
+import { StickiesComponent } from './stickies/stickies.component'
+
+export const appRoutes: Routes = [
+  { path: 'stickies', component: StickiesComponent },
+]
